a73da6a220476dac05f30060cba3d8171fa50833
unho.chang <capcock@snu.ac.kr>
2024-06-26 14:38:04 +0900

    금일 학습 마무리 커밋

--- Bandits.py
+++ Bandits.py
@@ -110,3 +110,31 @@ plt.ylabel("Rates")
 plt.xlabel("Steps")
 plt.plot(rates)
 plt.show()
+
+# 실험 200번 실행후 평균으로 알고리즘 성능을 평가함.
+runs = 200
+steps = 1000
+epsilon = 0.3
+all_rates = np.zeros((runs, steps))
+
+for run in range(runs):
+    bandit = Bandit()
+    agent = Agent(epsilon)
+    total_reward = 0
+    rates = []
+
+    for step in range(steps):
+        action = agent.get_action()
+        reward = bandit.play(action)
+        agent.update(action, reward)
+        total_reward += reward
+        rates.append(total_reward / (step + 1))
+
+    all_rates[run] = rates
+
+avg_rates = np.average(all_rates, axis=0)
+
+plt.ylabel("Rates")
+plt.xlabel("Steps")
+plt.plot(avg_rates)
+plt.show()
