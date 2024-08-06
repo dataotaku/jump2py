6c8736a3618fc17a080449519c05da6cffcf748f
unho.chang <capcock@snu.ac.kr>
2024-08-06 10:37:20 +0900

    feat: Add breast cancer classification example using KNeighborsClassifier

--- skleran_02.py
+++ skleran_02.py
@@ -7,5 +7,234 @@ from IPython.display import display
 import mglearn
 from sklearn.model_selection import train_test_split
 
+import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+import platform
+
+# 시스템에 따라 폰트 설정
+if platform.system() == "Windows":
+    # Windows의 경우
+    font_name = font_manager.FontProperties(
+        fname="c:/Windows/Fonts/malgun.ttf"
+    ).get_name()
+    rc("font", family=font_name)
+elif platform.system() == "Darwin":
+    # Mac의 경우
+    rc("font", family="AppleGothic")
+else:
+    # Linux의 경우
+    rc("font", family="NanumGothic")
+
+# 음수 기호가 깨지는 문제 해결
+rc("axes", unicode_minus=False)
+
 mglearn.plots.plot_linear_regression_wave()
+plt.show()
+
+from sklearn.linear_model import LinearRegression
+
+X, y = mglearn.datasets.make_wave(n_samples=60)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+lr = LinearRegression().fit(X_train, y_train)
+# help(LinearRegression.fit)
+
+print("lr.coef_:", lr.coef_)
+print("lr.intercept_:", lr.intercept_)
+print("훈련 세트 점수: {:.2f}".format(lr.score(X_train, y_train)))
+print("테스트 세트 점수: {:.2f}".format(lr.score(X_test, y_test)))
+
+X, y = mglearn.datasets.load_extended_boston()
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+lr = LinearRegression().fit(X_train, y_train)
+print("훈련 세트 점수: {:.2f}".format(lr.score(X_train, y_train)))
+print("테스트 세트 점수: {:.2f}".format(lr.score(X_test, y_test)))
+
+from sklearn.linear_model import Ridge
+
+ridge = Ridge().fit(X_train, y_train)
+print("훈련 세트 점수: {:.2f}".format(ridge.score(X_train, y_train)))
+print("테스트 세트 점수: {:.2f}".format(ridge.score(X_test, y_test)))
+
+ridge10 = Ridge(alpha=10).fit(X_train, y_train)
+print("훈련 세트 점수: {:.2f}".format(ridge10.score(X_train, y_train)))
+print("테스트 세트 점수: {:.2f}".format(ridge10.score(X_test, y_test)))
+
+ridge01 = Ridge(alpha=0.1).fit(X_train, y_train)
+print("훈련 세트 점수: {:.2f}".format(ridge01.score(X_train, y_train)))
+print("테스트 세트 점수: {:.2f}".format(ridge01.score(X_test, y_test)))
+
+plt.plot(ridge.coef_, "s", label="Ridge alpha=1")
+plt.plot(ridge10.coef_, "^", label="Ridge alpha=10")
+plt.plot(ridge01.coef_, "v", label="Ridge alpha=0.1")
+plt.plot(lr.coef_, "o", label="LinearRegression")
+plt.xlabel("계수 목록")
+plt.ylabel("계수 크기")
+xlims = plt.xlim()
+plt.hlines(0, xlims[0], xlims[1])
+plt.xlim(xlims)
+plt.ylim(-25, 25)
+plt.legend()
+plt.show()
+
+mglearn.plots.plot_ridge_n_samples()
+plt.show()
+
+from sklearn.linear_model import Lasso
+
+lasso = Lasso().fit(X_train, y_train)
+print("훈련 세트 점수: {:.2f}".format(lasso.score(X_train, y_train)))
+print("테스트 세트 점수: {:.2f}".format(lasso.score(X_test, y_test)))
+print("사용한 특성의 수:", np.sum(lasso.coef_ != 0))
+
+lasso001 = Lasso(alpha=0.01, max_iter=100000).fit(X_train, y_train)
+
+print("훈련 세트 점수: {:.2f}".format(lasso001.score(X_train, y_train)))
+print("테스트 세트 점수: {:.2f}".format(lasso001.score(X_test, y_test)))
+print("사용한 특성의 수:", np.sum(lasso001.coef_ != 0))
+print(lasso.n_iter_)
+
+lasso00001 = Lasso(alpha=0.0001, max_iter=100000).fit(X_train, y_train)
+print("훈련 세트 점수: {:.2f}".format(lasso00001.score(X_train, y_train)))
+print("테스트 세트 점수: {:.2f}".format(lasso00001.score(X_test, y_test)))
+print("사용한 특성의 수:", np.sum(lasso00001.coef_ != 0))
+print(lasso00001.n_iter_)
+
+plt.plot(lasso.coef_, "s", label="Lasso alpha=1")
+plt.plot(lasso001.coef_, "^", label="Lasso alpha=0.01")
+plt.plot(lasso00001.coef_, "v", label="Lasso alpha=0.0001")
+plt.plot(ridge01.coef_, "o", label="Ridge alpha=0.1")
+plt.legend(ncol=2, loc=(0, 1.05))
+plt.ylim(-25, 25)
+plt.xlabel("계수 목록")
+plt.ylabel("계수 크기")
+plt.show()
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+
+X, y = mglearn.datasets.make_forge()
+fig, axes = plt.subplots(1, 2, figsize=(10, 3))
+
+for model, ax in zip([LinearSVC(), LogisticRegression()], axes):
+    clf = model.fit(X, y)
+    mglearn.plots.plot_2d_separator(clf, X, fill=False, eps=0.5, ax=ax, alpha=0.7)
+    mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+    ax.set_title("{}".format(clf.__class__.__name__))
+    ax.set_xlabel("특성 0")
+    ax.set_ylabel("특성 1")
+axes[0].legend()
+plt.show()
+
+mglearn.plots.plot_linear_svc_regularization()
+plt.show()
+
+from sklearn.datasets import load_breast_cancer
+
+cancer = load_breast_cancer()
+X_train, X_test, y_train, y_test = train_test_split(
+    cancer.data, cancer.target, stratify=cancer.target, random_state=42
+)
+logreg = LogisticRegression(max_iter=10000).fit(X_train, y_train)
+print("훈련 세트 점수: {:.3f}".format(logreg.score(X_train, y_train)))
+print("테스트 세트 점수: {:.3f}".format(logreg.score(X_test, y_test)))
+
+logreg100 = LogisticRegression(C=100, max_iter=10000).fit(X_train, y_train)
+print("훈련 세트 점수: {:.3f}".format(logreg100.score(X_train, y_train)))
+print("테스트 세트 점수: {:.3f}".format(logreg100.score(X_test, y_test)))
+
+logreg001 = LogisticRegression(C=0.01, max_iter=10000).fit(X_train, y_train)
+print("훈련 세트 점수: {:.3f}".format(logreg001.score(X_train, y_train)))
+print("테스트 세트 점수: {:.3f}".format(logreg001.score(X_test, y_test)))
+
+plt.plot(logreg.coef_.T, "o", label="C=1")
+plt.plot(logreg100.coef_.T, "^", label="C=100")
+plt.plot(logreg001.coef_.T, "v", label="C=0.001")
+plt.xticks(range(cancer.data.shape[1]), cancer.feature_names, rotation=90)
+plt.hlines(0, 0, cancer.data.shape[1])
+plt.ylim(-5, 5)
+plt.xlabel("특성")
+plt.ylabel("계수 크기")
+plt.legend()
+plt.show()
+
+for C, marker in zip([0.001, 1, 100], ["o", "^", "v"]):
+    lr_l1 = LogisticRegression(
+        C=C, penalty="l1", solver="liblinear", max_iter=10000
+    ).fit(X_train, y_train)
+    print(
+        "C={:.3f} 인 l1 로지스틱 회귀의 훈련 정확도: {:.2f}".format(
+            C, lr_l1.score(X_train, y_train)
+        )
+    )
+    print(
+        "C={:.3f} 인 l1 로지스틱 회귀의 테스트 정확도: {:.2f}".format(
+            C, lr_l1.score(X_test, y_test)
+        )
+    )
+    plt.plot(lr_l1.coef_.T, marker, label="C={:.3f}".format(C))
+plt.xticks(range(cancer.data.shape[1]), cancer.feature_names, rotation=90)
+plt.hlines(0, 0, cancer.data.shape[1])
+plt.xlabel("특성")
+plt.ylabel("계수 크기")
+plt.ylim(-5, 5)
+plt.legend(loc=3)
+plt.show()
+
+from sklearn.datasets import make_blobs
+
+X, y = make_blobs(random_state=42)
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+plt.xlabel("특성 0")
+plt.ylabel("특성 1")
+plt.legend(["클래스 0", "클래스 1", "클래스 2"])
+plt.show()
+
+linear_svm = LinearSVC().fit(X, y)
+print("계수 배열의 크기: ", linear_svm.coef_.shape)
+print("절편 배열의 크기: ", linear_svm.intercept_.shape)
+
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+line = np.linspace(-15, 15)
+for coef, intercept, color in zip(
+    linear_svm.coef_, linear_svm.intercept_, mglearn.cm3.colors
+):
+    plt.plot(line, -(line * coef[0] + intercept) / coef[1], c=color)
+plt.ylim(-10, 15)
+plt.xlim(-10, 8)
+plt.xlabel("특성 0")
+
+plt.ylabel("특성 1")
+plt.legend(
+    [
+        "클래스 0",
+        "클래스 1",
+        "클래스 2",
+        "클래스 0 경계",
+        "클래스 1 경계",
+        "클래스 2 경계",
+    ],
+    loc=(1.01, 0.3),
+)
+plt.show()
+
+mglearn.plots.plot_2d_classification(linear_svm, X, fill=True, alpha=0.7)
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+line = np.linspace(-15, 15)
+for coef, intercept, color in zip(
+    linear_svm.coef_, linear_svm.intercept_, mglearn.cm3.colors
+):
+    plt.plot(line, -(line * coef[0] + intercept) / coef[1], c=color)
+plt.legend(
+    [
+        "클래스 0",
+        "클래스 1",
+        "클래스 2",
+        "클래스 0 경계",
+        "클래스 1 경계",
+        "클래스 2 경계",
+    ],
+    loc=(1.01, 0.3),
+)
+plt.xlabel("특성 0")
+plt.ylabel("특성 1")
 plt.show()
