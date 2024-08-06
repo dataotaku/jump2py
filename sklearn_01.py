c586ba4f8aea4e830eb0d15763e96d71727e6ef9
unho.chang <capcock@snu.ac.kr>
2024-08-06 09:05:30 +0900

    chore: Add breast cancer classification example using KNeighborsClassifier

--- sklearn_01.py
+++ sklearn_01.py
@@ -14,6 +14,7 @@ import sklearn
 from sklearn.datasets import make_blobs
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.datasets import load_breast_cancer
 
 
 x = np.array([[1, 2, 3], [4, 5, 6]])
@@ -231,4 +232,60 @@ for n_neighbors, ax in zip([1, 3, 9], axes):
     ax.set_xlabel("feature 0")
     ax.set_ylabel("feature 1")
 axes[0].legend(loc=3)
+plt.show()
+
+cancer = load_breast_cancer()
+X_train, X_test, y_train, y_test = train_test_split(
+    cancer.data, cancer.target, stratify=cancer.target, random_state=66
+)
+
+training_accuracy = []
+test_accuracy = []
+neighbors_settings = range(1, 11)
+
+for n_neighbors in neighbors_settings:
+    clf = KNeighborsClassifier(n_neighbors=n_neighbors)
+    clf.fit(X_train, y_train)
+    training_accuracy.append(clf.score(X_train, y_train))
+    test_accuracy.append(clf.score(X_test, y_test))
+
+plt.plot(neighbors_settings, training_accuracy, label="training accuracy")
+plt.plot(neighbors_settings, test_accuracy, label="test accuracy")
+plt.ylabel("Accuracy")
+plt.xlabel("n_neighbors")
+plt.legend()
+plt.show()
+
+mglearn.plots.plot_knn_regression(n_neighbors=1)
+plt.show()
+
+mglearn.plots.plot_knn_regression(n_neighbors=3)
+plt.show()
+
+from sklearn.neighbors import KNeighborsRegressor
+X, y = mglearn.datasets.make_wave(n_samples=40)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+reg = KNeighborsRegressor(n_neighbors=3)
+reg.fit(X_train, y_train)
+print("Test set predictions:\n{}".format(reg.predict(X_test)))
+print("Test set R^2: {:.2f}".format(reg.score(X_test, y_test)))
+
+fig, axes = plt.subplots(1, 3, figsize=(15, 4))
+line = np.linspace(-3, 3, 1000).reshape(-1, 1)
+line[-5:-1, :]
+
+for n_neighbors, ax in zip([1, 3, 9], axes):
+    reg = KNeighborsRegressor(n_neighbors=n_neighbors)
+    reg.fit(X_train, y_train)
+    ax.plot(line, reg.predict(line))
+    ax.plot(X_train, y_train, "^", c=mglearn.cm2(0), markersize=8)
+    ax.plot(X_test, y_test, "v", c=mglearn.cm2(1), markersize=8)
+    ax.set_title(
+        "{} neighbor(s)\n train score: {:.2f} test score: {:.2f}".format(
+            n_neighbors, reg.score(X_train, y_train), reg.score(X_test, y_test)
+        )
+    )
+    ax.set_xlabel("Feature")
+    ax.set_ylabel("Target")
+axes[0].legend(["Model predictions", "Training data/target", "Test data/target"], loc="best")
 plt.show()
---
+++ skleran_02.py
@@ -0,0 +1,11 @@
+import numpy as np
+from graphviz import Digraph
+from scipy import sparse
+import matplotlib.pyplot as plt
+import pandas as pd
+from IPython.display import display
+import mglearn
+from sklearn.model_selection import train_test_split
+
+mglearn.plots.plot_linear_regression_wave()
+plt.show()
